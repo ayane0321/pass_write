bdbef5f1c6ca066551215e2ddabee5143d0a6339
鈴木彩音 <ayane@ayanembp.local>
2019-10-25 23:35:16 +0900

    repair program

--- passw.py
+++ passw.py
@@ -1,10 +1,22 @@
 import os
+from pathlib import Path
+
+img_dir_name = input('Input image directory name:')
 
 #絶対パスを取得
-file = os.path.abspath("")
+file = os.path.abspath(img_dir_name)
 print(file)
 
 basename = os.path.basename(file)
 split = basename.split('.')
+
+#fileに入れた絶対パスのPathオブジェクトを生成
+ppath = Path(file)
+#file直下のファイルとディレクトリを取得してリストにする
+list1 = list(ppath.glob("*"))
+
 with open(split[0] + '.txt','w')as f:
-    f.write(file)
+    for tmp in list1:
+        list_lines = str(tmp)
+        f.write(list_lines)
+        f.write('\n')
